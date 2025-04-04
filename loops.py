d5e79d1abf6f5c41916c2c9cee7112d861fc2895
batreides08 <bquiroscamacho@byupathway.edu>
2025-04-03 20:03:05 -0600

    comm

--- loops.py
+++ loops.py
@@ -1,15 +1,11 @@
 payment = float(input("What is the payment amount? "))
 
-while payment < 0:
-    print("Sorry the payment can't be negative.")
+penalty = 0
 
+while payment < 0:
+    penalty = 1.50
+
+    print("Sorry the payment can't be negative.")
     payment = float(input("What is the payment amount? "))
 
-    print("...")
-    print("...")
-    print("...")
-    print("...")
-
-    #jump back to line 3
-
-print(f"The amount is ${payment:.2f}")
+print(f"The amount is ${payment:.2f}. The penalty is ${penalty:.2f}")
